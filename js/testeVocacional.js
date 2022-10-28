cd7d33e444f79bbca42ca97d952fd045435c43a7
Lucemy Ferreira <lucemyjunior@hotmail.com>
2022-10-28 16:27:00 -0300

    Adding info button

---
+++ js/testeVocacional.js
@@ -0,0 +1,10 @@
+const humanas = 0
+const exatas = 0
+const saude = 0
+
+const ask = document.getElementById('test-text')
+const option1 = document.getElementById('option1')
+const option2 = document.getElementById('option2')
+const option3 = document.getElementById('option3')
+const option4 = document.getElementById('option4')
+
